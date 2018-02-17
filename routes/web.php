a3bdd92ab166d8b18d6c019f4c328b0915e7eb6a
himanshi <himanshichauhan.st@gmail.com>
2018-02-17 08:57:33 +0530

    web.php

---
+++ routes/web.php
@@ -0,0 +1,35 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('home', function () {
+    return view('index');
+});
+Route::get('download', function () {
+    return view('download');
+});
+Route::get('features', function () {
+    return view('features');
+});
+Route::get('contact', function () {
+    return view('contact');
+});
+Route::get('form', function () {
+    return view('form');
+});
+Route::post('/customer/ajaxupdate','Controller@testfunction');
+
+
+//post('/reg', 'Controller@testfunction');
